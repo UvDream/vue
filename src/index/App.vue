43bbecb3a479b316e7dcc83a6faa6f4e9915a253
程查秋 <chengchaqiu@chengchaqiudeMacBook-Pro.local>
2019-08-20 08:54:51 +0800

    add home  control

--- src/index/App.vue
+++ src/index/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
-    index page!
+    <button @click="controllerBtn">控制屏</button>
+    <button @click="mapBtn">地图</button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    controllerBtn() {
+      location.href = "controller";
+    },
+    mapBtn() {
+      location.href = "map";
+    }
+  }
+};
 </script>
