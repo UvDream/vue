ed4a28b3181847c393dfe39c54260aad17b17439
cq360767996 <360767996@qq.com>
2019-09-10 14:47:25 +0800

    add all police

--- src/map/views/Police.vue
+++ src/map/views/Police.vue
@@ -9,14 +9,15 @@ import axios from "axios";
 export default {
   mounted() {
     let { name } = this.$route.query;
+    name = name || "";
     axios.post("/location/getPolicePoints", { name }).then(result => {
       // axios.get("/json/map/police/policePoints.json", { name }).then(result => {
       const data = result.data;
       const map = new ZTMAP.Map({
         container: "map",
-        center: data.center,
-        zoom: 13.2,
-        pitch: 50
+        center: name ? data.center : [120.96669037868332, 31.414776697732762],
+        zoom: name ? 13.2 : 11.5,
+        pitch: name ? 50 : 0
       });
       map.on("load", () => {
         data.data.map(val => {
